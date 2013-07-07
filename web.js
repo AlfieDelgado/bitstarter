eabf221a8f99305f1d0fdecdff7ef834207a692a
Alfredo Delgado <alfre2x@gmail.com>
2013-07-07 07:29:23 +0000

    fs y buffer prueba

--- web.js
+++ web.js
@@ -6,9 +6,9 @@ var buf = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   // response.send('Hello World 2!');
-   // response.send(fs.readFileSync("index.html"));
-    response.send(buf.toString());
+    response.send('Hello World 2!');
+    response.send('fs.readFileSync ' + fs.readFileSync("index.html"));
+    response.send('buf.toString ' + buf.toString());
 });
 
 var port = process.env.PORT || 5000;
