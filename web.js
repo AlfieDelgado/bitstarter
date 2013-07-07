12abc553687d59cf952a87f80029398152508b35
Alfredo Delgado <alfre2x@gmail.com>
2013-07-07 07:58:52 +0000

    buffer final

--- web.js
+++ web.js
@@ -1,21 +1,17 @@
 var fs=require('fs');
-var express = require('express');
 
-var buf = new Buffer(fs.readFileSync("index.html"));
+var buf = new Buffer(fs.readFileSync('index.html'));
+
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World 2!');
+    //response.send('Hello World 2!');
+    //response.send(fs.readFileSync("index.html", "utf-8"));
+    response.send(buf.toString());
 });
-app.get('/', function(request, response) {
-   response.send('fs.readFileSync ' + fs.readFileSync("index.html"));                  
-});
-app.get('/', function(request, response) {
-   // response.send('Hello World 2!');                                                  
-   // response.send('fs.readFileSync ' + fs.readFileSync("index.html"));              
-    response.send('buf.toString ' + buf.toString());
-});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
