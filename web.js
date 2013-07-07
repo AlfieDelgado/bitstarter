23771c12fcf6a5c86914d4f1b22a0b1f3e7ae9ad
Alfredo Delgado <alfre2x@gmail.com>
2013-07-07 06:12:35 +0000

    prueba index1

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var indexfile = fs.readFile("index.html");
+var indexbuf = new Buffer (indexfile, "utf-8");
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+    console.log(indexbuf);
 });
 
 var port = process.env.PORT || 5000;
